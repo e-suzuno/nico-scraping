9e7472d4f27481ab9714a7af3f7888accda1360b
eiji suzuno <e-suzuno@mediaglobe.jp>
2019-10-08 10:26:35 +0900

    リンクを別窓になるように修正

--- src/web/public/js/app.js
+++ src/web/public/js/app.js
@@ -1918,6 +1918,8 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "nicoComicItem",
   props: ['item'],
@@ -38745,15 +38747,25 @@ var render = function() {
         ? [_vm._v("\n        除外されました。\n    ")]
         : [
             _c("div", { staticClass: "nico-comic-list__item__title" }, [
-              _c("a", { attrs: { href: _vm.data.url } }, [
-                _vm._v(
-                  "[ " +
-                    _vm._s(_vm.data.nico_no) +
-                    " ]\n                " +
-                    _vm._s(_vm.data.title) +
-                    "\n            "
-                )
-              ]),
+              _c(
+                "a",
+                {
+                  attrs: {
+                    href: _vm.data.url,
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [
+                  _vm._v(
+                    "[ " +
+                      _vm._s(_vm.data.nico_no) +
+                      " ]\n                " +
+                      _vm._s(_vm.data.title) +
+                      "\n            "
+                  )
+                ]
+              ),
               _vm._v(" "),
               _c(
                 "button",
@@ -51229,15 +51241,14 @@ __webpack_require__.r(__webpack_exports__);
 /*!*************************************************************!*\
   !*** ./resources/js/components/nicoComic/nicoComicItem.vue ***!
   \*************************************************************/
-/*! no static exports found */
+/*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _nicoComicItem_vue_vue_type_template_id_6499b651_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./nicoComicItem.vue?vue&type=template&id=6499b651&scoped=true& */ "./resources/js/components/nicoComic/nicoComicItem.vue?vue&type=template&id=6499b651&scoped=true&");
 /* harmony import */ var _nicoComicItem_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./nicoComicItem.vue?vue&type=script&lang=js& */ "./resources/js/components/nicoComic/nicoComicItem.vue?vue&type=script&lang=js&");
-/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _nicoComicItem_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__) if(__WEBPACK_IMPORT_KEY__ !== 'default') (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _nicoComicItem_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[key]; }) }(__WEBPACK_IMPORT_KEY__));
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
 
 
@@ -51267,7 +51278,7 @@ component.options.__file = "resources/js/components/nicoComic/nicoComicItem.vue"
 /*!**************************************************************************************!*\
   !*** ./resources/js/components/nicoComic/nicoComicItem.vue?vue&type=script&lang=js& ***!
   \**************************************************************************************/
-/*! no static exports found */
+/*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
