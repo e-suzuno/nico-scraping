6da1bceacab07d527fd029db0aa6bc1e733d2fe6
eiji suzuno <e-suzuno@mediaglobe.jp>
2019-10-09 13:38:33 +0900

    Seederをちょっと修正

--- src/web/database/seeds/TagsTableSeeder.php
+++ src/web/database/seeds/TagsTableSeeder.php
@@ -12,12 +12,12 @@ class TagsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('tags')->delete();
-        
+
         \DB::table('tags')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
                 'label' => 'ユーザー',
@@ -25,7 +25,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-09-27 14:43:58',
                 'updated_at' => '2019-09-27 14:43:58',
             ),
-            1 => 
+            1 =>
             array (
                 'id' => 2,
                 'label' => '少年マンガ',
@@ -33,7 +33,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-09-27 14:41:30',
                 'updated_at' => '2019-09-27 14:41:30',
             ),
-            2 => 
+            2 =>
             array (
                 'id' => 3,
                 'label' => '少女マンガ',
@@ -41,7 +41,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-09-27 14:43:33',
                 'updated_at' => '2019-09-27 14:43:33',
             ),
-            3 => 
+            3 =>
             array (
                 'id' => 4,
                 'label' => '青年マンガ',
@@ -49,7 +49,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-09-27 14:43:35',
                 'updated_at' => '2019-09-27 14:43:35',
             ),
-            4 => 
+            4 =>
             array (
                 'id' => 5,
                 'label' => '4コママンガ',
@@ -57,7 +57,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-09-27 14:43:40',
                 'updated_at' => '2019-09-27 14:43:40',
             ),
-            5 => 
+            5 =>
             array (
                 'id' => 6,
                 'label' => 'その他マンガ',
@@ -65,7 +65,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-09-27 14:43:31',
                 'updated_at' => '2019-09-27 14:43:31',
             ),
-            6 => 
+            6 =>
             array (
                 'id' => 7,
                 'label' => 'ファンコミック',
@@ -73,7 +73,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-09-27 14:43:58',
                 'updated_at' => '2019-09-27 14:43:58',
             ),
-            7 => 
+            7 =>
             array (
                 'id' => 8,
                 'label' => '管理人おすすめ',
@@ -81,7 +81,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-10-04 02:17:42',
                 'updated_at' => '2019-10-04 02:17:43',
             ),
-            8 => 
+            8 =>
             array (
                 'id' => 9,
                 'label' => '異世界',
@@ -89,7 +89,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-10-03 16:36:39',
                 'updated_at' => '2019-10-03 16:36:39',
             ),
-            9 => 
+            9 =>
             array (
                 'id' => 10,
                 'label' => 'ファンタジー',
@@ -97,7 +97,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-10-03 16:36:41',
                 'updated_at' => '2019-10-03 16:36:41',
             ),
-            10 => 
+            10 =>
             array (
                 'id' => 11,
                 'label' => 'ロボット',
@@ -105,7 +105,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-10-03 16:42:47',
                 'updated_at' => '2019-10-03 16:42:47',
             ),
-            11 => 
+            11 =>
             array (
                 'id' => 15,
                 'label' => '完結',
@@ -113,7 +113,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-10-04 04:05:06',
                 'updated_at' => '2019-10-04 04:05:08',
             ),
-            12 => 
+            12 =>
             array (
                 'id' => 16,
                 'label' => '試し読み',
@@ -121,7 +121,7 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-10-08 01:05:36',
                 'updated_at' => '2019-10-08 01:05:37',
             ),
-            13 => 
+            13 =>
             array (
                 'id' => 17,
                 'label' => '暴力的描写',
@@ -129,16 +129,16 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2019-10-08 01:05:55',
                 'updated_at' => '2019-10-08 01:05:54',
             ),
-            14 => 
+            14 =>
             array (
                 'id' => 18,
-                'label' => '性的',
+                'label' => '性的描写',
                 'tag_type_id' => 5,
                 'created_at' => '2019-10-08 01:06:11',
                 'updated_at' => '2019-10-08 01:06:13',
             ),
         ));
-        
-        
+
+
     }
 }
